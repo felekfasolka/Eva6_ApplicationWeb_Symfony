b9c4d3297a42da48a2c27a97a3a048b10f8a2c3e
Felix B <work@uberleben.com>
2021-09-30 16:40:02 +0200

    initial import

---
+++ src/Controller/MissionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Mission;
+use App\Repository\MissionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class MissionController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(Request $request, Environment $twig, MissionRepository $missionRepository): Response
+    {
+        $words = ['sky', 'cloud', 'wood', 'rock', 'forest',
+            'mountain', 'breeze'];
+        return new Response($twig->render('mission/index.html.twig', [
+            'missions' => $missionRepository->findAll(),
+            'words' => $words
+        ]));
+    }
+
+    /**
+     * @Route ("/mission/{id}", name="mission")
+     */
+    public function show(Environment $twig, Mission $mission, MissionRepository $missionRepository): Response
+    {
+        return new Response($twig->render('mission/show.html.twig', [
+            'mission' => $mission,
+        ]));
+    }
+}
